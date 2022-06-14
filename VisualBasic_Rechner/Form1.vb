d3cc086b1af9c741862cb73e5f530ca851456684
Nico1220 <86952580+Nico1220@users.noreply.github.com>
2022-06-14 19:30:17 +0200

    Korrektur
    If-Statement korrigiert und Formel erstellt

--- VisualBasic_Rechner/Form1.vb
+++ VisualBasic_Rechner/Form1.vb
@@ -1,8 +1,10 @@
 ﻿Public Class Form1
 
-    Public Hight As String, Weight As String, PGrößeinP As String, PGewichtinP As String, Perzentilen As Double, Alter As Double, 
-    LGR As Double, LGW As Double, MGR As Double, MGW As Double, SGR As Double, SGW As Double
-    
+    Public Hight As String, Weight As String, PGrößeinP As String, PGewichtinP As String, PerzentilenHight As Double, PerzentilenWeight As Double,
+    AlterBack As String, LGR As Double, LGW As Double, MGR As Double, MGW As Double, SGR As Double, SGW As Double
+
+
+
 
     Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
 
@@ -17,8 +19,10 @@
     End Sub
 
     Private Sub Alter_SelectedIndexChanged(sender As Object, e As EventArgs) Handles Alter.SelectedIndexChanged
-        Alter = Alter_SelectedIndexChanged
-        
+        For Each item As Object In Alter.Items
+            AlterBack &= CStr(item)
+        Next
+
     End Sub
 
     Private Sub HightInCm_TextChanged(sender As Object, e As EventArgs) Handles HightInCm.TextChanged
@@ -48,12 +52,33 @@
 
     End Sub
 
-    if (Alter = "4 Monate" && RadioMale.CheckedChanged, LGR = 0.1285, MGR = 64.04, SGR = 0.039, LGW = 0.451, MGW = 6.84, SGW = 0.1206) 
-
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
-        Perzentilen = Weight * Hight
-        PGröße.Text = CStr(Perzentilen)
-        
+        If (AlterBack = "4 Monate" And RadioMale.Checked) Then
+
+            LGR = 0.1285
+            MGR = 64.04
+            SGR = 0.039
+            LGW = 0.451
+            MGW = 6.84
+            SGW = 0.1206
+
+        ElseIf (AlterBack = "5 Monate" And RadioMale.Checked) Then
+
+            LGR = 0.1119
+            MGR = 66.37
+            SGR = 0.0375
+            LGW = 0.3604
+            MGW = 7.45
+            SGW = 0.1186
+
+        End If
+
+
+        PerzentilenHight = ((Hight / MGR) / (LGR * SGR) * 100 / 100)
+        PGröße.Text = CStr(PerzentilenHight)
+        PerzentilenWeight = (((Weight / MGW) - 1) / (LGW * SGW) * 100 / 100)
+        PGewicht.Text = CStr(PerzentilenWeight)
+
     End Sub
 
 End Class
